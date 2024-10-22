dcbc517eed1d9e1dd5946a5852379c663c1b7265
N07H1NG <nikitos09012004@mail.ru>
2024-10-22 20:48:41 +0300

    a ton of changes

--- Assets/Prefabs/Player/PlayerControl.cs
+++ Assets/Prefabs/Player/PlayerControl.cs
@@ -4,6 +4,7 @@ using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Timeline;
 using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 public class PlayerControl : MonoBehaviour
@@ -15,32 +16,72 @@ public class PlayerControl : MonoBehaviour
     Vector3 inputVector;
     Vector2 mouseDelta;
     Vector3 lookVector;
+    Vector3 targetVector;
+    Vector3 dampVelocity = Vector3.zero;
     [SerializeField] Vector2 maxTurnSpeed;
+    [SerializeField] float nervousness=0.1f;
     CharacterController myController;
     Transform cameraTransform;
+    Transform actualCamera;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible =false;
         Cursor.lockState = CursorLockMode.Locked;
-        lookVector = transform.forward;
+        
         myController = GetComponent<CharacterController>();
         cameraTransform = gameObject.transform.GetChild(0);
+        actualCamera = cameraTransform.GetChild(0);
+        lookVector = cameraTransform.forward;
+        targetVector = cameraTransform.forward;
+        StartCoroutine(HeadBob());
     }
 
     // Update is called once per frame
     void Update()
     {
         inputVector = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical")).normalized;
-        mouseDelta = new Vector2(Input.GetAxis("Mouse X"),-1*Input.GetAxis("Mouse Y")) ;
-
+        mouseDelta = new Vector2(Input.GetAxis("Mouse X"),-1*Input.GetAxis("Mouse Y"));
+        //mouseDelta*= Time.deltaTime*100;
         mouseDelta = math.clamp(mouseDelta,-1*maxTurnSpeed,maxTurnSpeed);
-        gameObject.transform.Rotate(0,mouseDelta.x,0);
+        float d = Vector3.Dot(targetVector,Vector3.up);
+        if(math.abs(d) > 0.9f){
+            print("Close");
+            mouseDelta.y *= (mouseDelta.y*d>=0)?1:0;
+            print(mouseDelta.y);
+        }
+        targetVector = Quaternion.Euler(0,mouseDelta.x,0f) * targetVector;
+        targetVector = Quaternion.AngleAxis(mouseDelta.y,Vector3.Cross(Vector3.up,targetVector) )*targetVector;
+        lookVector = Vector3.SmoothDamp(lookVector,targetVector,ref dampVelocity,(5+Vector3.Dot(lookVector,targetVector)*nervousness)/20f);
+        transform.forward = new Vector3(lookVector.x,0,lookVector.z).normalized;
+        cameraTransform.forward = lookVector;
+        //transform.Rotate(axis: Vector3.up, angle: mouseDelta.x);
+        //print(mouseDelta.x);
+        //cameraTransform.transform.Rotate(axis: Vector3.right, angle: mouseDelta.y);
+        //cameraTransform.Rotate(cameraTransform.right,mouseDelta.y);
+        //cameraTransform.
+        //cameraTransform.forward = lookTransform.forward;
+        //gameObject.transform.forward =new Vector3(cameraTransform.forward.x,0,cameraTransform.forward.z).normalized;
         targetVelocity = transform.rotation* inputVector;
-        Vector3 diff = (targetVelocity-characterVelocity);
+        Vector3 diff = targetVelocity-characterVelocity;
         characterVelocity += diff.normalized*math.clamp(inertia*Time.deltaTime,0,diff.magnitude);
         myController.SimpleMove(characterVelocity*walkSpeed);
-        cameraTransform.Rotate(mouseDelta.y,0,0);
+        
+    }
+
+    IEnumerator HeadBob()
+    {
+        float timer=0;
+        Vector3 camDamp = Vector3.zero;
+        Vector3 targetPos;
+        while(true){
+            timer += 3f*walkSpeed*characterVelocity.magnitude*Time.deltaTime;
+            targetPos= cameraTransform.position + new Vector3(0,math.sin(timer)*0.8f,0)*characterVelocity.magnitude;
+            timer = timer%(2*math.PI);
+            actualCamera.position = Vector3.SmoothDamp(actualCamera.position,targetPos,ref camDamp,0.5f);
+            actualCamera.LookAt(cameraTransform.position + cameraTransform.forward*8f);
+            yield return null;
+        }
     }
 
     
--- Assets/movecar.cs
+++ Assets/movecar.cs
@@ -19,7 +19,7 @@ public class movecar : MonoBehaviour
     {
         start = transform.position;
         finish = start + transform.right*range;
-        print("Theyseemerolin");
+
         StartCoroutine(RideAround());
     }
 
@@ -28,14 +28,14 @@ public class movecar : MonoBehaviour
     {   
         
         while (true){
-            print("patrolin they trina catch me ridin dirty");
+            
             float p = Random.Range(0,2);
             float d = (p-0.5f)*-2f*speed*Random.Range(0.5f,1.5f);
-            print(p);
-            print(d);
-            print("they hatin");
+            
             transform.position = Vector3.Lerp(start,finish,p);
+            
             Vector3 trg = Vector3.Lerp(start,finish,1-p);
+            transform.forward = trg - transform.position;
             while (transform.position != trg){
                 
                 transform.position = Vector3.Lerp(start,finish,p);
